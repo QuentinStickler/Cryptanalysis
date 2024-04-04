ae349958033ff77adca243fc477b621da2b00e2c
QuentinStickler <quentin.stickler@yahoo.com>
2024-04-04 15:34:34 +0200

    Added De-and Encryption

--- rc4test.py
+++ rc4test.py
@@ -5,9 +5,9 @@ File for testing the RC4 algorithm and also trying to crack it
 '''
 
 ###Here, we initialize our S-Box with values from 0-255 and our T-Box, which just contains repeated values of our given key 256 times
-def initialize():
+def initialize(plaintext):
     S = [0] * 256
-    key = "I AM THE KEY"            ###Enter your key here
+    key = "Supersicherer Key"            ###Enter your key here
     asciikey = []
     T = [0] * 256
 
@@ -20,20 +20,59 @@ def initialize():
         S[x] = x
         T[x] = asciikey[x % keylength]
 
-    permutation(S,T)
+    permutation(S,T,plaintext)
 
 ###Here, we use our initialized S-Box and T-Box to permutate the S-Box
 ###It will then include all values from 0-256 still, but now in a (pseudo-)random order dependent on our given key
-def permutation(S,T):
+def permutation(S,T,plaintext):
     j = 0
     for i in range(256):
         j = (j + S[i] + T[i]) % 256
         currentvalue = S[i]
         S[i] = S[j]
         S[j] = currentvalue
+    generatestream(S, plaintext)
 
 
-def generatestream():
-    return None
+def generatestream(S, plaintext):
+    i = 0
+    j = 0
 
-initialize()
+    asciiplaintext = []
+    for c in plaintext:                   
+        asciiplaintext.append(ord(c))
+    plaintextlength = len(asciiplaintext)
+    keystream = []
+
+    for x in range(plaintextlength):
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        currentValue = S[i]
+        S[i] = S[j]
+        S[j] = currentValue
+        t = (S[i] + S[j]) % 256
+        keystream.append(S[t])
+
+    ciphertext = encrypt(asciiplaintext, keystream)
+    decrypt(ciphertext, keystream)
+
+def encrypt(asciiplaintext, keystream):
+
+    encryptedlist = []
+    for x,y in zip(asciiplaintext,keystream):
+        encryptedlist.append(chr(x^y))
+    return encryptedlist
+
+def decrypt(ciphertext, keystream):
+
+    decryptedlist = []
+    for x,y in zip(ciphertext,keystream):
+        decryptedlist.append(chr(ord(x)^y))
+    result = ""
+    for value in decryptedlist:
+        result += value
+    print(result)
+    return result
+
+plaintext = "Plaintext"
+initialize(plaintext)
