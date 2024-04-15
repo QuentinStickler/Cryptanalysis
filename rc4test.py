89403f2f9446331a0e87ebb975c038b13ed41389
QuentinStickler <quentin.stickler@yahoo.com>
2024-04-15 15:52:09 +0200

    Fixed code logic and style

--- rc4test.py
+++ rc4test.py
@@ -5,99 +5,101 @@ ___doc___ = '''
 File for testing the RC4 algorithm and also trying to crack it
 
 '''
-
-###Here, we initialize our S-Box with values from 0-255 and our T-Box, which just contains repeated values of our given key 256 times
-def initialize(plaintext, key):
-    S = [0] * 256
-    asciikey = []
-    T = [0] * 256
-
-    for c in key:                   ###Create ascii list from key 
-        asciikey.append(ord(c))
-    keylength = len(asciikey)
-
-
-    for x in range(256):            ###Initilaze S-Box and T-Box
-        S[x] = x
-        T[x] = asciikey[x % keylength]
-
-    return permutation(S,T,plaintext)
-
-###Here, we use our initialized S-Box and T-Box to permutate the S-Box
-###It will then include all values from 0-256 still, but now in a (pseudo-)random order dependent on our given key
-def permutation(S,T,plaintext):
+#region SecondImplmentation
+def initialize(key):
+    #Initialize S-box with values 0-255 and kbox with repeated key values
+    sbox = []
+    kbox = []
+    for x in range(256):
+        sbox[x] = x
+        kbox[x] = key[x % len(key)]
+        
     j = 0
     for i in range(256):
-        j = (j + S[i] + T[i]) % 256
-        currentvalue = S[i]
-        S[i] = S[j]
-        S[j] = currentvalue
-    return generatestream(S, plaintext)
+        #Permutate s-box with the given key
+        j = (j + sbox[i] + kbox[i]) % 256
+        
+        #Swap sbox[i] and sbox[j]
+        tmp = sbox[i]
+        sbox[i] = sbox[j]
+        sbox[j] = tmp
+    return sbox
+    
 
-
-def generatestream(S, plaintext):
+def stream_generation(sbox, text):
+    keystream = []
     i = 0
     j = 0
-
-    asciiplaintext = []
-    for c in plaintext:                   
-        asciiplaintext.append(ord(c))
-    plaintextlength = len(asciiplaintext)
-    keystream = []
-
-    for x in range(plaintextlength):
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        currentValue = S[i]
-        S[i] = S[j]
-        S[j] = currentValue
-        t = (S[i] + S[j]) % 256
-        keystream.append(S[t])
-    return keystream
-
-def encrypt(asciiplaintext, keystream):
-    encryptedlist = []
-    for x,y in zip(asciiplaintext,keystream):
-        encryptedlist.append(chr(ord(x)^y))
-
-    result = ""
-    for val in encryptedlist:
-        result += val
-    print(result)
+    #Generate random keystream with length of plaintext
+    for x in range(len(text)):
+        i = (1 + i) % 256
+        j = (sbox[i] + j) % 256
+        
+        #Swap sbox[i] and sbox[j]
+        tmp = sbox[j]
+        sbox[j] = sbox[i]
+        sbox[i] = tmp
+        
+        keystream.append(sbox[(sbox[i] + sbox[j]) % 256])    
+    return keystream    
 
 
-def decrypt(ciphertext, keystream):
-    decryptedlist = []
-    for x,y in zip(ciphertext,keystream):
-        decryptedlist.append(chr(ord(x)^y))
-    result = ""
-    for value in decryptedlist:
-        result += value
-    print(result)
+def encrypt(text, key):
+    text = [ord(char) for char in text]
+    
+    sbox = initialize(key)
+    key_stream = stream_generation(sbox, text)
+    
+    ciphertext = ''
+    i = 0
+    for char in text:
+        #Plaintext in Hex for better visualization
+        enc = str(hex(char ^ key_stream[i])).upper()
+        ciphertext += (enc)
+        i += 1
+        
+    return ciphertext
+    
+
+def decrypt(ciphertext, key):
+    #Crop out the "0X" from the Hex presentation
+    ciphertext = ciphertext.split('0X')[1:]
+    ciphertext = [int('0x' + c.lower(), 0) for c in ciphertext]
+    
+    sbox = initialize(key)
+    key_stream = stream_generation(sbox, ciphertext)
+    
+    plaintext = ''
+    i = 0
+    for char in ciphertext:
+        dec = str(chr(char ^ key_stream[i]))
+        plaintext += dec
+        i += 1
+    
+    return plaintext
 
 
-def main():
-    mode = input("Do you want to encrypt ('E') or decrypt ('D')?")
-    if(mode == "E"):
-        plaintext = input("Please enter the text you want to encrypt: ")
-        key = input("Pleas enter your key: ")
-        keystream = initialize(plaintext, key)
-        encrypt(plaintext, keystream)
-    elif(mode == "D"):
-        ciphertext = input("Please enter the text you want to decrypt: ")  
-        key = input("Pleas enter your key: ")
-        keystream = initialize(ciphertext, key)
-        decrypt(ciphertext, keystream)
+if __name__ == '__main__':
+    mode = input('Do you want to encrypt ("E") or Decrypt ("D")?: ').upper()
+    if mode == 'E':
+        plaintext = input('Please enter the text you want to encrypt: ')
+        key = input('Please enter you key: ')
+        key = [ord(char) for char in key]
+        result = encrypt(plaintext, key)
+        print('Result: ')
+        print(result)
+    elif mode == 'D': 
+        ciphertext = input('Please enter the text you want to decrypt: ')
+        key = input('Pleas enter your key: ')
+        key = [ord(char) for char in key]
+        result = decrypt(ciphertext, key)
+        print('Result: ')
+        print(result)
     else:
-        print("Wrong input")
-    return
+        print('Error in input - try again.')
+#endregion
 
-
-#main()
-
-
-####################TEST-AREA##############################
-
+#region Testing Ground 
 
 def testinitialize(plaintext, key):
     S = [0] * 256
@@ -109,7 +111,7 @@ def testinitialize(plaintext, key):
     keylength = len(asciikey)
 
 
-    for x in range(256):            ###Initilaze S-Box and T-Box
+    for x in range(256):            ###Initilaze sbox and T-Box
         S[x] = x
         T[x] = asciikey[x % keylength]
 
@@ -239,4 +241,5 @@ def recoverkeystreambyte(ivb1, ivb2, ivb3, ivb4, wantedbyte):
     else: print("IV doesn't work.")
 
 
-recoverkeystreambyte(4,255,123,69, 4)
+#recoverkeystreambyte(4,255,123,69, 4)
+#endregion
